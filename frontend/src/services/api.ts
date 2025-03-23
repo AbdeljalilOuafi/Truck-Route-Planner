38face9987f8496fa03c9eef366e51d1fd5ae79b
AbdeljalilOuafi <abdeljalilouafi44@gmail.com>
2025-03-23 03:37:47 +0000

    Update API_BASE_URL for the new url + update requirements.txt

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { RouteInput } from '../types';
 
-const API_BASE_URL = 'https://ouafi.tech/api';
+const API_BASE_URL = 'https://truck-route-planner.crafitori.com/api';
 
 export const calculateRoute = async (input: RouteInput) => {
   const response = await axios.post(`${API_BASE_URL}/calculate-route/`, input);
